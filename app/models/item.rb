c6f2caf96f36cabccee714efef56877c8535ed21
tmtocb <kittz.esport@gmail.com>
2021-01-06 00:06:09 +0100

    make progress bar work

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :list
+
+  scope :completed, ->  do
+    where(completed: true)    
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,17 @@
 class List < ApplicationRecord
   has_many :items
+
+  def total_items
+    @total_items ||= items.count
+  end
+
+  def completed_items
+    @completed_items ||= items.completed.count
+  end
+
+  def percent_complete
+    return 0 if total_items == 0
+    (100 * completed_items.to_f / total_items).round(1)
+  end
+
 end
