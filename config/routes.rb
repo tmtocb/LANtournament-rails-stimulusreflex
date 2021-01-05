aa5c99c8d5bf2b84c641d1fb129efc5e1c38b0fd
tmtocb <kittz.esport@gmail.com>
2021-01-05 16:26:27 +0100

    style layout, lists index/show, add SCSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists do
+   resources :lists do
     resources :items
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'lists#index'
 end
