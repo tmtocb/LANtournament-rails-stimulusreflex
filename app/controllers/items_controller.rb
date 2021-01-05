4f5bbc15b416ce2c4a0ae91ebad61966cf440faa
tmtocb <kittz.esport@gmail.com>
2021-01-06 00:44:38 +0100

    change input into form, redirect to list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,36 +2,30 @@ class ItemsController < ApplicationController
   before_action :set_list
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  # GET lists/1/items
   def index
     @items = @list.items
   end
 
-  # GET lists/1/items/1
   def show
   end
 
-  # GET lists/1/items/new
   def new
     @item = @list.items.build
   end
 
-  # GET lists/1/items/1/edit
   def edit
   end
 
-  # POST lists/1/items
   def create
     @item = @list.items.build(item_params)
 
     if @item.save
-      redirect_to([@item.list, @item], notice: 'Item was successfully created.')
+      redirect_to(@item.list)
     else
       render action: 'new'
     end
   end
 
-  # PUT lists/1/items/1
   def update
     if @item.update_attributes(item_params)
       redirect_to([@item.list, @item], notice: 'Item was successfully updated.')
@@ -40,7 +34,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE lists/1/items/1
   def destroy
     @item.destroy
 
@@ -48,7 +41,6 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_list
       @list = List.find(params[:list_id])
     end
@@ -57,7 +49,6 @@ class ItemsController < ApplicationController
       @item = @list.items.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def item_params
       params.require(:item).permit(:description, :completed, :completed_at, :list_id)
     end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,28 +1,21 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
 
-  # GET /lists
-  # GET /lists.json
   def index
     @lists = List.all
   end
 
-  # GET /lists/1
-  # GET /lists/1.json
   def show
+    @item = Item.new
   end
 
-  # GET /lists/new
   def new
     @list = List.new
   end
 
-  # GET /lists/1/edit
   def edit
   end
 
-  # POST /lists
-  # POST /lists.json
   def create
     @list = List.new(list_params)
 
@@ -37,8 +30,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lists/1
-  # PATCH/PUT /lists/1.json
   def update
     respond_to do |format|
       if @list.update(list_params)
@@ -51,8 +42,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # DELETE /lists/1
-  # DELETE /lists/1.json
   def destroy
     @list.destroy
     respond_to do |format|
@@ -62,12 +51,10 @@ class ListsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_list
       @list = List.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def list_params
       params.require(:list).permit(:title)
     end
