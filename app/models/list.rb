737fcfdcb458ad50f566e3b12c548ab9179c1aab
tmtocb <kittz.esport@gmail.com>
2021-01-06 01:06:34 +0100

    add badge with case of completion

--- app/models/list.rb
+++ app/models/list.rb
@@ -14,4 +14,26 @@ class List < ApplicationRecord
     (100 * completed_items.to_f / total_items).round(1)
   end
 
+  def status
+    case percent_complete.to_i
+      when 0
+        'not started'
+      when 100
+        'completed'
+      else 
+        'In progress'
+    end
+  end
+
+  def badge_color
+    case percent_complete.to_i
+      when 0
+        "badge-no"
+      when 100
+        "badge-yes"
+      else 
+        "badge-progress"
+     end
+  end
+
 end
